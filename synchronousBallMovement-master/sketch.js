62ec906c804d67aa653d53dcf3f7311f1c8d71df
cavseaglespm22107 <70402866+cavseaglespm22107@users.noreply.github.com>
2020-11-15 13:31:39 -0500

    Add files via upload

---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,26 @@
+var gameState = 0;
+var playerCount;
+var database;
+var form, player, game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  /*if(gameState === 1){
+    clear();
+    game.play();
+  }*/
+}
+
